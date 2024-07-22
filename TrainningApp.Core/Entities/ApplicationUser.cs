ab9f0d6d6bf7810d8b3480f06cb857b39e598721
joaop <joaopaulo2692@gmail.com>
2024-07-22 15:14:13 -0300

    fix - alteracoes

---
+++ TrainningApp.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainningApp.Core.Entities
+{
+    internal class ApplicationUser : IdentityUser
+    {
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public float? Heigth { get; set; }
+        public string? Gender { get; set; }
+        public float? Weight { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+        [Column("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+        [Column("disabled_at")]
+        public DateTime? DisabledAt { get; set; }
+    }
+}
