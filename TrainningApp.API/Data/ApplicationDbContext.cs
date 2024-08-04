5a807d67fe94fe1b20be64d2db682e1895273304
joaop <joaopaulo2692@gmail.com>
2024-08-03 21:20:36 -0300

    feat - adicionado novas entidades

--- TrainningApp.API/Data/ApplicationDbContext.cs
+++ TrainningApp.API/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,11 +10,31 @@ using TrainningApp.Core.Entities;
 
 namespace TrainningApp.Infrastructure.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+        {
+        }
+        public DbSet<Trainning> Trainnings { get; set; } 
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
+            modelBuilder.Entity<ApplicationUser>()
+               .HasMany(u => u.Trainnings)
+               .WithMany(t => t.Users)
+               .UsingEntity(j => j.ToTable("UserTrainnings"));
+
+            modelBuilder.Entity<ApplicationUser>()
+              .HasMany(u => u.Managements)
+              .WithMany(t => t.Users)
+              .UsingEntity(j => j.ToTable("UserManagements"));
+
+            //modelBuilder.Entity<ApplicationUser>().HasMany(t => t.Trainnings).WithMany(x => x.Users);
+            //modelBuilder.Entity<Trainning>().HasMany(x => x.Users).WithMany(x => x.Trainnings).UsingEntity(j => j.ToTable("UserTrainning"));
         }
     }
 }
--- TrainningApp.Core/Entities/Management.cs
+++ TrainningApp.Core/Entities/Management.cs
@@ -4,26 +4,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 
 namespace TrainningApp.Core.Entities
 {
-    public class ApplicationUser : IdentityUser
+    public class Management
     {
-        public string? Name { get; set; }
-        public int? Age { get; set; }
-        public float? Heigth { get; set; }
-        public string? Gender { get; set; }
-        public float? Weight { get; set; }
-
+        public int Id { get; set; }
+        public string Name { get; set; }
         [Column("created_at")]
         public DateTime CreatedAt { get; set; }
         [Column("updated_at")]
         public DateTime UpdatedAt { get; set; }
         [Column("disabled_at")]
         public DateTime? DisabledAt { get; set; }
-
-        public virtual List<Trainning> Trainnings { get; set; }
-        public virtual List<Management> Managements { get; set; }
+        public virtual List<ApplicationUser> Users { get; set; }
     }
 }
