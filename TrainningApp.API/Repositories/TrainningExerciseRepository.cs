6be29014ebb139ecff967192fbd0693739e6758d
joaop <joaopaulo2692@gmail.com>
2024-10-13 18:20:31 -0300

    feat - implementado repositorio de TrainningExercise

--- TrainningApp.API/Repositories/TrainningExerciseRepository.cs
+++ TrainningApp.API/Repositories/TrainningExerciseRepository.cs
@@ -1,39 +1,78 @@
 ﻿using FluentResults;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainningApp.Core.DTO.TrainningExercise;
 using TrainningApp.Core.Entities;
 using TrainningApp.Core.RepositoriesInterface;
+using TrainningApp.Infrastructure.Data;
 
 namespace TrainningApp.Infrastructure.Repositories
 {
     public class TrainningExerciseRepository : ITrainningExerciseRepository
     {
-        public Task<Result> CreateAsync(TrainningExercise trainningExercise)
+        private readonly ApplicationDbContext _db;
+
+        public TrainningExerciseRepository(ApplicationDbContext db)
         {
-            throw new NotImplementedException();
+            _db = db;
         }
 
-        public Task<Result> DeleteAsync(TrainningExercise trainningExercise)
+        public async Task<Result> CreateAsync(TrainningExercise trainningExercise)
         {
-            throw new NotImplementedException();
+            trainningExercise.UpdatedAt = DateTime.Now;
+            trainningExercise.CreatedAt = DateTime.Now;
+
+            _db.TrainningExercises.Add(trainningExercise);
+
+            await _db.SaveChangesAsync();
+
+            return Result.Ok().WithSuccess(trainningExercise.Id.ToString());
         }
 
-        public Task<TrainningExercise> FindByIdAsync(int trainningExerciseId)
+        public async Task<Result> DeleteAsync(TrainningExercise trainningExercise)
         {
-            throw new NotImplementedException();
+            TrainningExercise dbTrainningExercise = await _db.TrainningExercises.Where(x => x.Id == trainningExercise.Id).FirstOrDefaultAsync();
+            dbTrainningExercise.UpdatedAt = DateTime.Now;
+            dbTrainningExercise.DisabledAt = DateTime.Now;
+
+            await _db.SaveChangesAsync();
+
+            return Result.Ok();
         }
 
-        public Task<List<TrainningExercise>> FindByIdsAsync(List<int> trainningExerciseIds)
+        public async Task<TrainningExercise> FindByIdAsync(int trainningExerciseId)
         {
-            throw new NotImplementedException();
+            TrainningExercise dbTrainningExercise = await _db.TrainningExercises.Where(x => x.Id == trainningExerciseId).FirstOrDefaultAsync();
+
+            return dbTrainningExercise;
         }
 
-        public Task<Result> UpdateAsync(TrainningExercise trainningExercise)
+        public async Task<List<TrainningExercise>> FindByIdsAsync(List<int> trainningExerciseIds)
         {
-            throw new NotImplementedException();
+            List<TrainningExercise> trainningExercises = new List<TrainningExercise>();
+            List<TrainningExercise> dbTrainningExercises = await _db.TrainningExercises.Where(x => x.Equals(trainningExerciseIds)).ToListAsync();
+
+            return dbTrainningExercises;
+        }
+
+        public async Task<Result> UpdateAsync(TrainningExercise trainningExercise)
+        {
+            TrainningExercise dbTrainningExercise = await _db.TrainningExercises.Where(x => x.Id == trainningExercise.Id).FirstOrDefaultAsync();
+            dbTrainningExercise.UpdatedAt = DateTime.Now;
+            dbTrainningExercise.Reps = trainningExercise.Reps;
+            dbTrainningExercise.Set = trainningExercise.Set;
+            dbTrainningExercise.Ordenation = trainningExercise.Ordenation;
+            dbTrainningExercise.Info = trainningExercise.Info;
+            dbTrainningExercise.Exercise = trainningExercise.Exercise;
+            dbTrainningExercise.TrainningDay = trainningExercise.TrainningDay;
+
+            await _db.SaveChangesAsync();
+
+            return Result.Ok();
         }
     }
 }
