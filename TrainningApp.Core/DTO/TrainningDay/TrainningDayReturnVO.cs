1f5aec5546afb1d39c258fc02d2d3747d346e757
joaop <joaopaulo2692@gmail.com>
2024-11-03 16:27:44 -0300

    fix - alteracao

---
+++ TrainningApp.Core/DTO/TrainningDay/TrainningDayReturnVO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainningApp.Core.DTO.TrainningExercise;
+
+namespace TrainningApp.Core.DTO.TrainningDay
+{
+    public class TrainningDayReturnVO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<TrainningExerciseVO> TrainningExercises { get; set; } 
+        public int TrainningId { get; set; }
+    }
+}
