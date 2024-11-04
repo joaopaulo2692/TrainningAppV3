c0db6d54c51ab0485c3346040c1a02a90b45e665
joaop <joaopaulo2692@gmail.com>
2024-11-04 15:58:51 -0300

    fix - alteracoes

---
+++ TrainningAppV3/TrainningAppV3.Client/Service/RefreshService.cs
@@ -0,0 +1,20 @@
+﻿using TrainningApp.Core.DTO.TrainningExercise;
+
+namespace TrainningAppV3.Client.Service
+{
+    public class RefreshService
+    {
+        public event Action RefreshRequested;
+        public TrainningExerciseVO TrainningExerciseVO { get; set; }
+
+        public void Execute(TrainningExerciseVO trainningExerciseVO)
+        {
+            TrainningExerciseVO = trainningExerciseVO;
+            RefreshRequested?.Invoke();
+        }
+        public void CallRequestRefresh()
+        {
+            RefreshRequested?.Invoke();
+        }
+    }
+}
