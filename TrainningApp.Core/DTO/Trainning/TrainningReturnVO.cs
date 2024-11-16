56adaf0d05b64969ec37a35dee768525c1d9651f
joaop <joaopaulo2692@gmail.com>
2024-11-15 22:06:06 -0300

    feat - criada tela de varios treinos do cliente

---
+++ TrainningApp.Core/DTO/Trainning/TrainningReturnVO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainningApp.Core.DTO.Trainning
+{
+    public class TrainningReturnVO
+    {
+        public int Id { get; set; }
+        public string Goal { get; set; }
+        public string Name { get; set; }
+        public string PersonalName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
