06556703cc9b0f8e0d841457b1c4ee695e501fda
joaop <joaopaulo2692@gmail.com>
2024-11-18 13:38:49 -0300

    feat - criado pagina clientes

--- TrainningApp.Core/DTO/User/UserVO.cs
+++ TrainningApp.Core/DTO/User/UserVO.cs
@@ -10,5 +10,7 @@ namespace TrainningApp.Core.DTO.User
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
     }
 }
