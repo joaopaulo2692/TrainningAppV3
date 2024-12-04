6a1b0ab3842ceb4a240f7abacb8e0ef56b2e6535
joaop <joaopaulo2692@gmail.com>
2024-12-04 14:21:36 -0300

    alt

--- TrainningAppV3/TrainningAppV3.Client/wwwroot/service-worker.js
+++ TrainningAppV3/TrainningAppV3.Client/wwwroot/service-worker.js
@@ -1,23 +1,4 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('trainning-app-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/css/app.css',
-                '/_framework/blazor.webassembly.js',
-                '/_framework/blazor.boot.json',
-                '/images/icons/icon-192x192.png',
-                '/images/icons/icon-512x512.png'
-            ]);
-        })
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            return cachedResponse || fetch(event.request);
-        })
-    );
-});
+// In development, always fetch from the network and do not enable offline support.
+// This is because caching would make development more difficult (changes would not
+// be reflected on the first load after each change).
+self.addEventListener('fetch', () => { });
