6605d18cbc3bb4c0e5d6b5d6bdce864e40162eaa
joaop <joaopaulo2692@gmail.com>
2024-12-08 21:46:32 -0300

    alteracao Yml e index

--- TrainningAppV3/TrainningAppV3.Client/obj/Release/net8.0/TrainningAppV3.Client.AssemblyInfo.cs
+++ TrainningAppV3/TrainningAppV3.Client/obj/Release/net8.0/TrainningAppV3.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainningAppV3.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daa5cd9695ac06e9e8251a7bd436ed5466707e6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05a1fe379ae3ddbb004f25482edc727a73600519")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainningAppV3.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainningAppV3.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
