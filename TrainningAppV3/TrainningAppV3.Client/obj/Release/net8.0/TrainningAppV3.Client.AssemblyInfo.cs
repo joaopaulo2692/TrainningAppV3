75ad8cb74a75b4bf6214352f6fdc8bdcd391e2f3
joaop <joaopaulo2692@gmail.com>
2024-12-08 21:37:53 -0300

    alteracao main

--- TrainningAppV3/TrainningAppV3.Client/obj/Release/net8.0/TrainningAppV3.Client.AssemblyInfo.cs
+++ TrainningAppV3/TrainningAppV3.Client/obj/Release/net8.0/TrainningAppV3.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainningAppV3.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6168e2e902b1c91491e31cc9c7755e6606c256")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daa5cd9695ac06e9e8251a7bd436ed5466707e6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainningAppV3.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainningAppV3.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
